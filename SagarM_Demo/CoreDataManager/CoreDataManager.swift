592699d7b5c48556d7a54668f7de8f713d62c5b6
sagarmahindrakar <41262371+sagarmahindrakar7395@users.noreply.github.com>
2026-02-17 23:05:31 +0530

    core data manager added

--- SagarM_Demo/CoreDataManager/CoreDataManager.swift
+++ SagarM_Demo/CoreDataManager/CoreDataManager.swift
@@ -5,4 +5,68 @@
 //  Created by APPLE on 12/19/25.
 //
 
+import CoreData
+import Foundation
+
+class CoreDataManager {
+    
+    static let shared = CoreDataManager()
+    
+    let persistentContainer: NSPersistentContainer
+    
+    private init() {
+        
+        persistentContainer = NSPersistentContainer(name: "SagarM_Demo")
+        
+        let storeURL = NSPersistentContainer.defaultDirectoryURL().appendingPathComponent("SagarM_Demo.sqlite")
+        
+        let storeDire = storeURL.deletingLastPathComponent()
+        
+        do {
+            
+            if FileManager.default.fileExists(atPath: storeDire.path) {
+                try FileManager.default.createDirectory(at: storeDire, withIntermediateDirectories: true)
+            }
+            
+            if let desc = persistentContainer.persistentStoreDescriptions.first {
+                desc.url = storeURL
+            }
+            
+            persistentContainer.loadPersistentStores { _, error in
+                if let error = error {
+                    fatalError("Error")
+                }
+            }
+            
+        } catch {
+            fatalError("Failed to creat directory and from URL")
+        }
+    }
+    
+    
+    func savePortfolioHolding(_ holdings: [UserHoldingElement], completion: ((Error?) -> Void)? = nil) {
+        let context = persistentContainer.newBackgroundContext()
+        
+        context.perform {
+            let fetchReq = NSFetchRequest<NSFetchRequestResult>(entityName: "HoldingEntity")
+            let deleteReq = NSBatchDeleteRequest(fetchRequest: fetchReq)
+            
+            do {
+                try context.execute(deleteReq)
+                
+            } catch {
+                print("Error to delete")
+            }
+            
+            for holding in holdings {
+                let entity = HoldingEntity(context: <#T##NSManagedObjectContext#>)
+            }
+            
+        }
+    }
+    
+    
+}
+
+
 
